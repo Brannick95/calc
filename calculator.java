d5be729bdfaa65242903ffa206ce1724d9efbead
Brannick95 <124726483+Brannick95@users.noreply.github.com>
2023-03-27 19:09:31 -0700

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,24 @@
+public class calculator {
+    int num1;
+    int num2;
+
+    public int add() {
+        return num1 + num2;
+    }
+
+    public int sub() {
+        return num1 - num2;
+    }
+
+    public int mult() {
+        return num1 * num2;
+    }
+
+    public int div() {
+        return num1 / num2;
+    }
+
+    public int sqr() {
+        return num1 * num1;
+    }
+}
